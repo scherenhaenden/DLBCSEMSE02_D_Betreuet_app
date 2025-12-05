f46e6bceefd7a5745141f6c8d3dcc88fea5a75fe
scherenhaenden <scherenhaenden@gmail.com>
2025-12-05 13:53:26 +0100

    feat: Update deployment target selector and simplify compileSdk configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,9 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.betreuer_app"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.betreuer_app"
