58b6c8021621c00a3cc83546e00dd84fab8d2f01
scherenhaenden <scherenhaenden@gmail.com>
2025-12-26 17:14:05 +0100

    feat: add UserRole class with user and role associations

---
+++ app/src/main/java/com/example/betreuer_app/model/UserRole.java
@@ -0,0 +1,42 @@
+package com.example.betreuer_app.model;
+
+import java.util.UUID;
+
+public class UserRole {
+    private UUID userId;
+    private User user;
+    private UUID roleId;
+    private Role role;
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public UUID getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(UUID roleId) {
+        this.roleId = roleId;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
